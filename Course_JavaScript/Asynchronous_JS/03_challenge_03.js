1e1ea4841f3426fc149532d6f4a6d2876ee893a1
Alexandre Sobota-Linux <alexandre.sobota@hotmail.com>
2020-06-26 14:08:46 -0300

    fourth module finished

---
+++ Course_JavaScript/Asynchronous_JS/03_challenge_03.js
@@ -0,0 +1,71 @@
+// Exercício 01
+const checaIdade = idade => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            idade >= 18 ? resolve() : reject();
+        }, 2000);
+    });
+};
+
+let anos = 27;
+checaIdade(anos)
+    .then(function () {
+        console.log(`Maior que 18, tem ${anos} anos, e mais alguns dias!`);
+    })
+    .catch(function () {
+        console.log(`Menor que 18, tem ${anos} anos, e mais alguns dias!`);
+    });
+
+// Exercício 02
+var inputName = document.getElementById("user");
+var dataList = document.querySelector("ul");
+const getUserRepo = name => {
+    var user = inputName.value;
+    if (!user) {
+        renderError();
+        alert("Preencha o campo");
+    }
+    renderLoading();
+    axios
+        .get(`https://api.github.com/users/${user}/repos`)
+        .then(response => {
+            fillList(response.data);
+        })
+        .catch(error => {
+            alert("Não foi possível efetuar a busca!");
+            renderError(error);
+        });
+};
+
+function renderLoading(loading) {
+    dataList.innerHTML = "";
+    var textElement = document.createTextNode("Carregando...");
+    var loadingElement = document.createElement("li");
+    loadingElement.appendChild(textElement);
+    dataList.appendChild(loadingElement);
+}
+
+function renderError(loading) {
+    dataList.innerHTML = "";
+    var user = inputName.value;
+    var msgUserEmpty = !user ? "Preencha o usuário" : "Erro ao efetuar busca";
+
+    var textElement = document.createTextNode(msgUserEmpty);
+    var errorElement = document.createElement("li");
+    errorElement.style.color = "#F00";
+    errorElement.appendChild(textElement);
+    dataList.appendChild(errorElement);
+}
+
+const fillList = repositorios => {
+    console.log("TCL: repositorios", repositorios);
+    dataList.innerHTML = "";
+
+    for (repo of repositorios) {
+        const reponame = document.createTextNode(repo.name);
+        const repoItem = document.createElement("li");
+
+        repoItem.appendChild(reponame);
+        dataList.appendChild(repoItem);
+    }
+};
