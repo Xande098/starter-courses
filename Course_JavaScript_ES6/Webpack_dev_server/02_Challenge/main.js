0c04b388f41111ba2481acb7e4ea4b342abe4607
Alexandre Sobota-Linux <alexandre.sobota@hotmail.com>
2020-06-30 20:33:49 -0300

    second module

---
+++ Course_JavaScript_ES6/Webpack_dev_server/02_Challenge/main.js
@@ -0,0 +1,15 @@
+// 1.1
+//import ClasseUsuario, { idade as IdadeUsuario } from './functions';
+
+//ClasseUsuario.info();
+
+// 1.2
+//import { idade } from './funtions';
+
+//console.log(idade);
+
+// 1.3
+import ClasseUsuario_2, { idade as IdadeUsuario } from './functions';
+
+ClasseUsuario_2.info();
+console.log(IdadeUsuario);
